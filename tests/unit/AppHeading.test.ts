monarch-initiative/monarch-ui-new

commit 5c3728dfaf80bc1eed73c3c1d588b40a2b53c69d

heading directive to heading component

src/components/AppHeading.vue
@@ -1,0 +1,122 @@
+<template>
+  <component :is="tag || 'div'" ref="heading">
+    <!-- heading content -->
+    <slot />
+
+    <!-- heading anchor -->
+    <AppLink
+      v-if="link"
+      :to="'#' + link"
+      class="anchor"
+      :aria-label="'Link to this section'"
+    >
+      <AppIcon icon="link" />
+    </AppLink>
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { kebabCase } from "lodash";
+
+// set heading level and tag, i.e. h1, h2, h3, etc
+function setTag(this: Heading) {
+  let level;
+  // if level manually specified, use that
+  if (this.level) {
+    level = this.level;
+  }
+  // otherwise, determine level of heading based on its position in document
+  else {
+    // heading element
+    const element = this?.$refs?.heading as HTMLElement;
+    // parent section element
+    const section = element.closest("section") as HTMLElement;
+
+    // if heading is first element in section
+    if (element.matches(":first-child")) {
+      // if section is first section in <main> and if no h1s already
+      // (only one h1 per page for accessibility)
+      if (section.matches(":first-child") && !document.querySelector("h1"))
+        level = 1;
+      // if section is latter
+      else level = 2;
+    }
+    // if heading is latter
+    else {
+      level = 3;
+    }
+  }
+
+  // set actual tag/component
+  this.tag = "h" + level;
+}
+
+// determine hash link
+function setLink(this: Heading) {
+  // heading element
+  const element = this?.$refs?.heading as HTMLElement;
+  // parent section element
+  const section = element.closest("section") as HTMLElement;
+
+  // if heading right at top of page, don't have link because no point
+  if (element.matches(":first-child") && section.matches(":first-child"))
+    this.link = "";
+  // otherwise, determine link from text content of heading
+  else this.link = kebabCase(element.innerText);
+}
+
+// heading component with anchor link and (optionally) automatic level
+const heading = defineComponent({
+  props: {
+    // manually specified heading level
+    level: Number,
+  },
+  data() {
+    return {
+      // tag of heading
+      tag: "",
+      // hash link of heading
+      link: "",
+    };
+  },
+  mounted() {
+    setTag.call(this);
+    setLink.call(this);
+  },
+  updated() {
+    // setTag.call(this);
+    // setLink.call(this);
+  },
+});
+
+// type of instance of this component
+type Heading = InstanceType<typeof heading>;
+
+// export component as usual
+export default heading;
+</script>
+
+<style lang="scss" scoped>
+.anchor {
+  display: inline-block;
+  width: 0;
+  margin-left: 10px;
+  opacity: 0;
+  text-decoration: none;
+  font-size: 0.8em;
+  transition: opacity $fast, color $fast;
+}
+
+.anchor:focus {
+  opacity: 1;
+}
+
+h1,
+h2,
+h3 {
+  &:hover .anchor {
+    opacity: 1;
+  }
+}
+</style>

src/directives/heading.ts
@@ -1,26 +1,0 @@
-import { kebabCase } from "lodash";
-
-// heading directive
-// usage: v-heading
-// adds a kebab-case id to an element and link button
-
-// when element created
-const mounted = (element: HTMLElement): void => {
-  // make id for heading based on contents
-  const id = kebabCase(element.getAttribute("id") || element.innerText);
-  element.setAttribute("id", id);
-
-  // make anchor link
-  const anchor = document.createElement("a");
-  anchor.innerHTML = "🔗";
-  anchor.classList.add("anchor");
-  anchor.href = "#" + id;
-  element.append(anchor);
-};
-
-// create directive
-const directive = {
-  mounted,
-};
-
-export default directive;

src/global/components.ts
@@ -1,5 +1,6 @@
 import { Component } from "vue";
 import AppSection from "@/components/AppSection.vue";
+import AppHeading from "@/components/AppHeading.vue";
 import AppLink from "@/components/AppLink.vue";
 import AppIcon from "@/components/AppIcon.vue";
 import AppButton from "@/components/AppButton.vue";
@@ -18,6 +19,7 @@ import AppCheckbox from "@/components/AppCheckbox.vue";
 // list of components we want to be available in any vue file without importing them
 const globalComponents: Record<string, Component> = {
   AppSection,
+  AppHeading,
   AppLink,
   AppIcon,
   AppButton,

src/global/icons.ts
@@ -1,5 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
+  faLink,
   faBars,
   faTimes,
   faBalanceScale,
@@ -33,6 +34,7 @@ import Loading from "@/assets/icons/Loading.vue";
 // create collection/"palette" of useable icons
 
 // general
+library.add(faLink);
 library.add(faBars);
 library.add(faTimes);
 library.add(faBalanceScale);

src/global/styles.scss
@@ -106,30 +106,6 @@ strong {
   font-weight: 600;
 }
 
-// anchors
-
-.anchor {
-  display: inline-block;
-  width: 0;
-  margin-left: 10px;
-  opacity: 0;
-  text-decoration: none;
-  font-size: 0.8em;
-  transition: opacity $fast;
-}
-
-.anchor:focus {
-  opacity: 1;
-}
-
-h1,
-h2,
-h3 {
-  &:hover .anchor {
-    opacity: 1;
-  }
-}
-
 // tooltips
 
 .tippy-box {

src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <AppSection design="fill">
-    <h2>Explore</h2>
+    <AppHeading>Explore</AppHeading>
     <AppInput placeholder="Search for something" />
   </AppSection>
   <AppSection>
-    <h2 v-heading>What is Monarch?</h2>
+    <AppHeading>What is Monarch?</AppHeading>
     <AppPlaceholder />
     <AppButton to="/about" text="Learn more" icon="arrow-right" />
   </AppSection>
   <AppSection>
-    <h2 v-heading>How to use Monarch</h2>
+    <AppHeading>How to use Monarch</AppHeading>
     <AppGallery>
       <AppPlaceholder />
       <AppPlaceholder />
@@ -20,7 +20,7 @@
     </AppGallery>
   </AppSection>
   <AppSection>
-    <h2 v-heading>Who's using Monarch</h2>
+    <AppHeading>Who's using Monarch</AppHeading>
     <AppGallery>
       <AppPlaceholder />
       <AppPlaceholder />

src/views/about/About.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- brief overview -->
   <AppSection>
-    <h1>About</h1>
+    <AppHeading>About</AppHeading>
     <p>
       The Monarch Initiative is an extensive, comprehensive knowledge graph and
       ecosystem of tools made for the benefit of clinicians, researchers, and
@@ -48,7 +48,7 @@
 
   <!-- social media -->
   <AppSection>
-    <h2 v-heading>Stay up to date</h2>
+    <AppHeading>Stay up to date</AppHeading>
     <p class="center">
       Be the first to know when we have important updates or other fun news.
     </p>
@@ -84,7 +84,7 @@
 
   <!-- mission statement -->
   <AppSection>
-    <h2 v-heading>Our Vision</h2>
+    <AppHeading>Our Vision</AppHeading>
     <ol>
       <li>
         Connect cross-species gene, genotype, variant, disease, and phenotype

src/views/about/Cite.vue
@@ -1,11 +1,11 @@
 <template>
   <AppSection>
-    <h1>Cite</h1>
+    <AppHeading>Cite</AppHeading>
   </AppSection>
 
   <!-- general acknowledgement -->
   <AppSection>
-    <h2 v-heading>Acknowledgement</h2>
+    <AppHeading>Acknowledgement</AppHeading>
     <p class="center">
       If you use Monarch, please acknolwedge us with a paragraph like the
       following:
@@ -21,7 +21,7 @@
 
   <!-- typical citing -->
   <AppSection>
-    <h2 v-heading>In Publications</h2>
+    <AppHeading>In Publications</AppHeading>
     <p class="center">
       To cite the use of Monarch in publications, please use these two
       references:
@@ -48,7 +48,7 @@
 
   <!-- citing data -->
   <AppSection>
-    <h2 v-heading>Data or Result</h2>
+    <AppHeading>Data or Result</AppHeading>
     <p>
       Because most of the association content in Monarch comes from external
       sources, please cite the original database records (e.g.
@@ -71,7 +71,7 @@
 
   <!-- citing pages -->
   <AppSection>
-    <h2 v-heading>Page or Algorithmic Results</h2>
+    <AppHeading>Page or Algorithmic Results</AppHeading>
     <p>
       To link to a specific page or to algorithmic results presented on this
       website, please use the URL in your browser's address bar or from the

commit f3a976727ffd16c536d5187396e1d98f2cc3ba4d

Add heading tests and testbed (#52)
- fix bug where id not added to heading (regression from https://github.com/monarch-initiative/monarch-ui-new/pull/51/commits/5c3728dfaf80bc1eed73c3c1d588b40a2b53c69d)
- add heading link to topmost h1 afterall
- change `innerText` to `textContent` for support in jest (jsdom)
- add section of routes for dev-only test routes
- make testbed page for testing out and experimenting with component look and feel
- make test for app heading component to see that auto level and link work

src/components/AppGallery.vue
@@ -51,7 +51,8 @@ $cell: minmax(100px, 1fr);
   }
 }
 
+// force no margins for children since grid already provides gap
 .gallery :deep(*) {
-  margin: 0;
+  margin: 0 !important;
 }
 </style>

src/components/AppHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag || 'div'" ref="heading">
+  <component :is="tag || 'div'" ref="heading" :id="link">
     <!-- heading content -->
     <slot />
 
@@ -30,27 +30,22 @@ function setTag(this: Heading) {
   }
 
   // otherwise, determine automatically based on heading's position in document
+  // https://dequeuniversity.com/rules/axe/4.1/page-has-heading-one
   else {
     // heading element
     const element = this?.$refs?.heading as HTMLElement;
-    // parent section element
-    const section = element.closest("section") as HTMLElement;
-    // is there an existing h1 (besides self)
-    let h1 = document.querySelector("h1");
-    if (h1 === element) h1 = null;
 
-    // if heading is first element in section
-    if (element.matches(":first-child")) {
-      // if section is first section in <main> and if no other h1s already
-      // (only one h1 per page for accessibility)
-      if (section.matches(":first-child") && !h1) level = 1;
-      // if section is latter
-      else level = 2;
-    }
-    // if heading is latter
-    else {
-      level = 3;
-    }
+    // if heading is first in parent
+    const firstInParent = element.matches("*:first-child");
+    // if heading is first in document
+    const firstInDoc = element.matches(
+      "main > section:first-child > *:first-child, main > *:first-child"
+    );
+
+    // determine level
+    if (firstInDoc) level = 1;
+    else if (firstInParent) level = 2;
+    else level = 3;
   }
 
   // set tag/component
@@ -61,14 +56,9 @@ function setTag(this: Heading) {
 function setLink(this: Heading) {
   // heading element
   const element = this?.$refs?.heading as HTMLElement;
-  // parent section element
-  const section = element.closest("section") as HTMLElement;
 
-  // if heading right at top of page, don't have link because no point
-  if (element.matches(":first-child") && section.matches(":first-child"))
-    this.link = "";
-  // otherwise, determine link from text content of heading
-  else this.link = kebabCase(element.innerText);
+  // determine link from text content of heading
+  this.link = kebabCase(element.textContent || "");
 }
 
 // heading component with anchor link and (optionally) automatic level

src/router/index.ts
@@ -15,6 +15,7 @@ import Publications from "@/views/about/Publications.vue";
 import Sources from "@/views/about/Sources.vue";
 import Terms from "@/views/about/Terms.vue";
 import Help from "@/views/help/Help.vue";
+import Testbed from "@/views/Testbed.vue";
 import { sleep } from "@/util/debug";
 
 // handle redirect from 404
@@ -84,6 +85,17 @@ export const routes: Array<RouteRecordRaw> = [
     name: "Help",
     component: Help,
   },
+
+  // test routes (pages to only include during development)
+  ...(process.env.NODE_ENV === "development"
+    ? [
+        {
+          path: "/testbed",
+          name: "Testbed",
+          component: Testbed,
+        },
+      ]
+    : []),
 ];
 
 const scrollBehavior: RouterScrollBehavior = async (

src/views/Testbed.vue
@@ -1,0 +1,28 @@
+<!--
+  dev page for experimenting with design and behavior of components. also a 
+  place for seeing all variations at once to check for coherence. not included
+  in production build of site.
+-->
+<template>
+  <!-- status component -->
+  <AppSection>
+    <AppGallery>
+      <AppStatus code="loading" text="Loading some results" />
+      <AppStatus code="success" text="Action was a success" />
+      <AppStatus code="warning" text="Be careful" />
+      <AppStatus code="error" text="There was an error" />
+      <AppStatus code="paused" text="Action is paused" />
+      <AppStatus code="unknown" text="Unexpected result" />
+    </AppGallery>
+  </AppSection>
+  <AppSection>
+    <AppGallery>
+      <AppStatus code="loading" design="big" text="Loading some results" />
+      <AppStatus code="success" design="big" text="Action was a success" />
+      <AppStatus code="warning" design="big" text="Be careful" />
+      <AppStatus code="error" design="big" text="There was an error" />
+      <AppStatus code="paused" design="big" text="Action is paused" />
+      <AppStatus code="unknown" design="big" text="Unexpected result" />
+    </AppGallery>
+  </AppSection>
+</template>

src/views/about/publications.yaml
@@ -216,7 +216,7 @@
       issue: "2:32"
       link: "[pubmed](http://www.ncbi.nlm.nih.gov/pubmed/31119199)"
 
-- year: 2019
+- year: 2018
   list:
     - title: "Classification, Ontology, and Precision Medicine (2018)"
       authors: "Haendel MA, Chute CG, Robinson PN 2018"

tests/unit/AppHeading.test.ts
@@ -1,0 +1,45 @@
+import { sleep } from "./../../src/util/debug";
+import { flushPromises, mount } from "@vue/test-utils";
+import AppHeadings from "./AppHeadings.vue";
+import { mountOptions } from "../setup";
+
+const tags = "h1, h2, h3, h4";
+
+test("Chooses heading levels correctly", async () => {
+  // mount hoc
+  const wrapper = mount(AppHeadings, mountOptions);
+
+  // wait for everything to render
+  await flushPromises();
+
+  // find all heading components
+  const headings = wrapper.findAll(tags);
+
+  // go through each
+  for (const heading of headings) {
+    // compare expected tag to actual rendered tag
+    const received = heading.element.tagName.toLowerCase();
+    const expected = heading.attributes("data-tag")?.toLowerCase();
+    expect(received).toBe(expected);
+  }
+});
+
+test("Creates heading links correctly", async () => {
+  // mount hoc
+  const wrapper = mount(AppHeadings, mountOptions);
+
+  // wait for everything to render
+  await flushPromises();
+  await sleep();
+
+  // find all heading components
+  const headings = wrapper.findAll(tags);
+
+  // go through each
+  for (const heading of headings) {
+    // compare expected link to actual rendered link
+    const received = heading.attributes("id");
+    const expected = heading.attributes("data-link");
+    expect(received).toBe(expected);
+  }
+});

tests/unit/AppHeadings.vue
@@ -1,0 +1,17 @@
+<template>
+  <!-- 
+    higher-order component (hoc) to test heading auto-levels/links. use data
+    attributes to indicate expected.
+  -->
+  <main>
+    <AppSection>
+      <AppHeading data-tag="h1" data-link="abc-def-gih">ABC-def_gih</AppHeading>
+    </AppSection>
+    <AppSection>
+      <AppHeading data-tag="h2" data-link="123-alphabet-street">
+        123 Alphabet Street
+      </AppHeading>
+      <AppHeading data-tag="h3" data-link="aeo">áéó</AppHeading>
+    </AppSection>
+  </main>
+</template>