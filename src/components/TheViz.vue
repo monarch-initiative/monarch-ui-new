83afb7f3c38a8930236bfeb884a6ff38c0a7ecec
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-11-23 17:34:47 -0500

    make viz 3d

--- src/components/TheViz.vue
+++ src/components/TheViz.vue
@@ -6,34 +6,45 @@
 import { defineComponent } from "vue";
 import { debounce } from "lodash";
 import { mix } from "tinycolor2";
-import { sin, cos, dist } from "@/util/math";
+import {
+  sin,
+  cos,
+  dist,
+  Point3d,
+  Point2d,
+  translate,
+  project,
+  getMidpoint,
+} from "@/util/math";
 
 // settings shared across functions
 const size = 4; // size of dots and links
 const gap = 50; // distance between dots
-const baseColor = "#" + mix("#ffffff", "#00838f", 92).toHex(); // color of elements at rest
+const baseColor = "#" + mix("#ffffff", "#00838f", 95).toHex(); // color of elements at rest
 const pulseColor = "#" + mix("#ffffff", "#00838f", 50).toHex(); // color of element when pulsing
 // (other one-off settings in place below)
 
+// 3d axis rotations
+let rx = 0;
+let ry = 0;
+
 interface Dot {
-  // rendered position
-  x: number;
-  y: number;
-  // center/original position
-  cx: number;
-  cy: number;
-  // angle and spin
-  angle: number;
-  spin: number;
+  // position in 3d space
+  point: Point3d;
+  // 3d position projected into 2d for canvas rendering
+  projected?: Point2d;
   // target and actual color
   color: string;
   colorTarget: string;
+  // angle and spin
+  angle: number;
+  spin: number;
 }
 
 interface Link {
   // dots to link together
-  a: Dot;
-  b: Dot;
+  from: Dot;
+  to: Dot;
   // target and actual color
   color: string;
   colorTarget: string;
@@ -58,11 +69,14 @@ const step = () => {
 const pulse = () => {
   for (const entity of [...dots, ...links]) {
     // get center position of entity
-    const x = "x" in entity ? entity.x : (entity.a.x + entity.b.x) / 2;
-    const y = "y" in entity ? entity.y : (entity.a.y + entity.b.y) / 2;
+    const center =
+      "point" in entity
+        ? entity.point
+        : getMidpoint(entity.from.point, entity.to.point);
+
     // time delays
     const speed = 2 / 10; // how fast pulse propagates outward
-    const start = dist(x - width / 2, y - height / 2) / speed;
+    const start = dist(center.x - width / 2, center.y - height / 2) / speed;
     const reset = start + 100 / speed;
     // set timers
     window.setTimeout(() => (entity.colorTarget = pulseColor), start);
@@ -92,36 +106,60 @@ const draw = () => {
   if (!ctx) return;
 
   // draw links
-  for (const { a, b, color } of links) {
+  for (const { from, to, color } of links) {
+    if (!from.projected || !to.projected) continue;
     ctx.strokeStyle = color;
     ctx.lineWidth = size / 3;
     ctx.beginPath();
-    ctx.moveTo(a.x, a.y);
-    ctx.lineTo(b.x, b.y);
+    ctx.moveTo(from.projected.x, from.projected.y);
+    ctx.lineTo(to.projected.x, to.projected.y);
     ctx.stroke();
   }
 
   // draw dots
-  for (const { x, y, color } of dots) {
+  for (const { projected, color } of dots) {
+    if (!projected) continue;
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2 * Math.PI);
+    ctx.arc(projected.x, projected.y, size, 0, 2 * Math.PI);
     ctx.fill();
   }
 };
 
+// rotate 3d world
+const rotate = (event: MouseEvent | TouchEvent) => {
+  const x = "clientY" in event ? event.clientY : event.touches[0].clientY;
+  const y = "clientX" in event ? event.clientX : event.touches[0].clientX;
+  rx = (0.5 - x / window.innerHeight) * 135;
+  ry = -(0.5 - y / window.innerWidth) * 90;
+};
+
 // move physics simulation one step
 const move = () => {
+  // for each dot
   for (const dot of dots) {
     // giving dots actual velocity makes them wander off and become less uniformly distributed
     // instead, move in circles around center point to give illusion of random movement
     dot.angle += dot.spin;
-    dot.x = dot.cx + (sin(dot.angle) * gap) / 4;
-    dot.y = dot.cy + (cos(dot.angle) * gap) / 4;
+    const wander = {
+      x: (cos(dot.angle) * gap) / 4,
+      y: (sin(dot.angle) * gap) / 4,
+      z: 0,
+    };
+
+    // project from 3d into 2d using 3d rotation matrix
+    dot.projected = project(
+      translate(dot.point, wander),
+      rx,
+      ry,
+      width / 2,
+      height / 2
+    );
   }
 
-  // move color toward target color smoothly
+  // for each entity (dot or link)
   const speed = 10;
+  // move color toward target color smoothly
   for (const entity of [...dots, ...links])
     entity.color = "#" + mix(entity.color, entity.colorTarget, speed).toHex();
 };
@@ -136,16 +174,17 @@ const generate = debounce(() => {
       if (Math.random() > 0.5)
         dots.push({
           // set positions
-          x,
-          y,
-          cx: x,
-          cy: y,
-          // set random starting angle and spin
-          angle: Math.random() * 360,
-          spin: -1 + Math.random() * 2,
+          point: {
+            x,
+            y,
+            z: -gap + Math.random() * 2 * gap,
+          },
           // set colors
           color: baseColor,
           colorTarget: baseColor,
+          // set random starting angle and spin
+          angle: Math.random() * 360,
+          spin: -0.5 + Math.random() * 1,
         });
     }
   }
@@ -154,20 +193,23 @@ const generate = debounce(() => {
   while (dots.length > 200)
     dots.splice(Math.floor(dots.length * Math.random()), 1);
 
+  // sort dots by z
+  dots.sort((a, b) => a.point.z - b.point.z);
+
   // go through each pair of dots
   links = [];
   for (let a = 0; a < dots.length; a++) {
     for (let b = 0; b < dots.length; b++) {
       // upper triangular
       if (a > b) {
-        const { cx: x1, cy: y1 } = dots[a];
-        const { cx: x2, cy: y2 } = dots[b];
+        const from = dots[a];
+        const to = dots[b];
         // only link if within a certain distance
-        if (dist(x1, y1, x2, y2) < gap * 2)
+        if (dist(from.point.x, from.point.y, to.point.x, to.point.y) < gap * 2)
           links.push({
             // set linked dots
-            a: dots[a],
-            b: dots[b],
+            from,
+            to,
             // set colors
             color: baseColor,
             colorTarget: baseColor,
@@ -177,11 +219,13 @@ const generate = debounce(() => {
   }
 
   // eliminate dots with no links
-  dots = dots.filter((dot) => links.find(({ a, b }) => dot === a || dot === b));
+  dots = dots.filter((dot) =>
+    links.find(({ from, to }) => dot === from || dot === to)
+  );
 }, 50);
 
 // fps
-window.setInterval(step, 1000 / 30);
+window.setInterval(step, 1000 / 60);
 // how frequently field pulses
 window.setInterval(pulse, 5000);
 
@@ -199,6 +243,10 @@ export default defineComponent({
       // regenerate field when container size changes
       generate();
     }).observe(canvas);
+
+    // listen for mouse move
+    window.addEventListener("mousemove", rotate);
+    window.addEventListener("touchmove", rotate);
   },
 });
 </script>
