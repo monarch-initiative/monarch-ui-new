862d0f72798f1b451944b5aadbe0699734aff790
Vince Rubinetti <vince.rubinetti@gmail.com>
2022-08-10 13:12:55 -0400

    Revert "add escape hatch to delete all node search history"
    This reverts commit 3295888b918f972278de3d4acb3e6e0848150f50.

--- src/components/AppSelectAutocomplete.vue
+++ src/components/AppSelectAutocomplete.vue
@@ -122,7 +122,7 @@ interface Emits {
   /** when input value change "submitted"/"committed" by user */
   (event: "change", value: string): void;
   /** when user wants to delete an entry */
-  (event: "delete", value: string, all?: boolean): void;
+  (event: "delete", value: string): void;
 }
 
 const emit = defineEmits<Emits>();
@@ -200,8 +200,7 @@ async function onKeydown(event: KeyboardEvent) {
 
   /** delete key to delete the highlighted result */
   if (event.key === "Delete" && event.shiftKey) {
-    const all = event.altKey && event.ctrlKey;
-    emit("delete", results.value[highlighted.value].name, all);
+    emit("delete", results.value[highlighted.value].name);
     await getResults();
   }
 
