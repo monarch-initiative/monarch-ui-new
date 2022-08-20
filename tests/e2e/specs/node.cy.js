1505a778bff93221ddc7b69ae423d1cc42d4c80a
Vince Rubinetti <vince.rubinetti@gmail.com>
2022-08-19 22:09:39 -0400

    reduce wait calls, uncomment tests

--- tests/e2e/specs/node.cy.js
+++ tests/e2e/specs/node.cy.js
@@ -1,59 +1,59 @@
-// it("Table of contents works", () => {
-//   cy.visit("/disease/MONDO:0007947");
+it("Table of contents works", () => {
+  cy.visit("/disease/MONDO:0007947");
 
-//   /** toggle button exists */
-//   cy.get(".toc")
-//     .invoke("attr", "aria-label")
-//     .should("include", "table of contents");
+  /** toggle button exists */
+  cy.get(".toc")
+    .invoke("attr", "aria-label")
+    .should("include", "table of contents");
 
-//   /** starts closed (due to small screen size) */
-//   cy.get(".toc button")
-//     .invoke("attr", "aria-expanded")
-//     .should("equal", "false");
+  /** starts closed (due to small screen size) */
+  cy.get(".toc button")
+    .invoke("attr", "aria-expanded")
+    .should("equal", "false");
 
-//   /** click button to open */
-//   cy.get(".toc").trigger("click");
-//   cy.get(".toc button").invoke("attr", "aria-expanded").should("equal", "true");
+  /** click button to open */
+  cy.get(".toc").trigger("click");
+  cy.get(".toc button").invoke("attr", "aria-expanded").should("equal", "true");
 
-//   /** click off to close (with small screen size) */
-//   cy.get("body").click(500, 500);
-//   cy.get(".toc button")
-//     .invoke("attr", "aria-expanded")
-//     .should("equal", "false");
+  /** click off to close (with small screen size) */
+  cy.get("body").click(500, 500);
+  cy.get(".toc button")
+    .invoke("attr", "aria-expanded")
+    .should("equal", "false");
 
-//   /** open again and check contents */
-//   cy.get(".toc").trigger("click");
-//   cy.contains(".toc", "Overview");
-//   cy.contains(".toc", "Hierarchy");
-//   cy.contains(".toc", "Associations");
+  /** open again and check contents */
+  cy.get(".toc").trigger("click");
+  cy.contains(".toc", "Overview");
+  cy.contains(".toc", "Hierarchy");
+  cy.contains(".toc", "Associations");
 
-//   /** check if solo selection mode works */
-//   cy.get(
-//     ".toc .checkbox"
-//   ).click(); /** cypress click function needed here for some reason */
-//   cy.get("main").contains("Overview").should("be.visible");
-//   cy.get("main").contains("Hierarchy").should("not.be.visible");
-//   cy.get("main").contains("Associations").should("not.be.visible");
-//   cy.get(".toc .checkbox").click();
-//   cy.get("main").contains("Hierarchy").should("be.visible");
-//   cy.get("main").contains("Associations").should("be.visible");
-// });
+  /** check if solo selection mode works */
+  cy.get(
+    ".toc .checkbox"
+  ).click(); /** cypress click function needed here for some reason */
+  cy.get("main").contains("Overview").should("be.visible");
+  cy.get("main").contains("Hierarchy").should("not.be.visible");
+  cy.get("main").contains("Associations").should("not.be.visible");
+  cy.get(".toc .checkbox").click();
+  cy.get("main").contains("Hierarchy").should("be.visible");
+  cy.get("main").contains("Associations").should("be.visible");
+});
 
-// it("Title info shows", () => {
-//   cy.visit("/disease/MONDO:0007947");
+it("Title info shows", () => {
+  cy.visit("/disease/MONDO:0007947");
 
-//   cy.contains("Marfan syndrome");
-//   cy.contains("Disease");
-//   cy.contains("MONDO:0007947");
-// });
+  cy.contains("Marfan syndrome");
+  cy.contains("Disease");
+  cy.contains("MONDO:0007947");
+});
 
-// it("Overview items show", () => {
-//   cy.visit("/disease/MONDO:0007947");
+it("Overview items show", () => {
+  cy.visit("/disease/MONDO:0007947");
 
-//   cy.contains("MFS1");
-//   cy.contains("Marfan syndrome type 1");
-//   cy.contains("Marfan syndrome is a disorder of the connective tissue");
-// });
+  cy.contains("MFS1");
+  cy.contains("Marfan syndrome type 1");
+  cy.contains("Marfan syndrome is a disorder of the connective tissue");
+});
 
 it("Details items show", () => {
   cy.visit("/disease/MONDO:0007947");
@@ -224,20 +224,12 @@ it("Evidence table viewer works", () => {
 
 it("Breadcrumbs section works", () => {
   cy.visit("/disease/MONDO:0007947");
-  cy.wait(500);
 
   cy.contains("Dural ectasia").click();
-  cy.wait(500);
   cy.contains("Cachexia").click();
-  cy.wait(500);
   cy.contains("syndromic myopia").click();
-  cy.wait(500);
-  cy.reload();
-  cy.wait(500);
   cy.contains("High, narrow palate").click();
-  cy.wait(500);
   cy.contains("Genu recurvatum").click();
-  cy.wait(500);
 
   cy.get("#breadcrumbs ~ .flex").as("breadcrumbs");
 
@@ -251,16 +243,16 @@ it("Breadcrumbs section works", () => {
     );
 
   cy.go(-4);
-  cy.wait(500);
+  cy.wait(100);
 
   cy.get("@breadcrumbs")
     .then(getInnerText)
     .should("eq", "Marfan syndrome Has Phenotype Dural ectasia");
 
   cy.go(3);
-  cy.wait(500);
+  cy.wait(100);
   cy.reload();
-  cy.wait(500);
+  cy.wait(100);
 
   cy.get("@breadcrumbs")
     .then(getInnerText)
@@ -270,7 +262,7 @@ it("Breadcrumbs section works", () => {
     );
 
   cy.get("@breadcrumbs").contains("Dural ectasia").click();
-  cy.wait(500);
+  cy.wait(100);
   cy.get("@breadcrumbs")
     .then(getInnerText)
     .should("eq", "Marfan syndrome Has Phenotype Dural ectasia");
